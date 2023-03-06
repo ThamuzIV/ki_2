55ea6156fa7c43aef0874f1555985f594162c1ac
ThamuzIV <you@example.com>
2023-03-06 09:51:04 +0700

    update code

--- oop/day2/haha.java
+++ oop/day2/haha.java
@@ -1,5 +1,16 @@
+import java.time.Clock;
+import java.time.LocalTime;
+
 public class haha {
     public static void main(String[] args) {
-        
+        LocalTime now = LocalTime.now();
+        System.out.println(now);
+        for (int i = 0; i < 100; i++) {
+            System.out.print("hi ");
+        }
+        System.out.println();
+        LocalTime now2 = LocalTime.now();
+       
+
     }
 }
--- oop/day4/src/find_min.java
+++ oop/day4/src/find_min.java
@@ -1,5 +1,4 @@
-import java.io.InputStream;
-import java.util.Scanner;
+
 
 public class find_min {
     public static void main(String[] args) {
